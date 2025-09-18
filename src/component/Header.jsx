47b141898d781c8bd32fbfa64142c83cbd29593b
Yashue <yashkatyura@gmail.com>
2025-09-18 12:53:06 +0530

    headerdone

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,15 +3,25 @@ import React from 'react'
 const Header = () => {
     return (
         <>
-            <div>
-                <div className=' ml-20 h-[100px] w-[80%] border-2 rounded-4xl flex items-center gap-10  '>
+            <div className='flex cursor-pointer'>
+                <div className=' ml-30 mt-4 h-[70px] w-[80%] border-2 rounded-4xl flex items-center gap-10  '>
+                    <div className='w-[20%]'>
                     <img src="src\assets\logo.png" className='h-[100px] w-[100px]' alt="" />
+                    </div>
+                    <div className='flex w-[45%] gap-10'>
                     <div>Home</div>
                     <div>Pages</div>
                     <div>Services</div>
                     <div>Case Studies</div>
                     <div>Blog</div>
+                    </div>
+                    <div className='w-[25%] flex gap-10'>
+                    <div>852452622</div>
+                    <div>Schedule an Appointment </div>
+                    </div>
+                    
                 </div>
+
             </div>
         </>
     )
