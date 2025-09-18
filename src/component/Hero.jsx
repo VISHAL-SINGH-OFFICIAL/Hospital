72b3c8c573391610726ec3cb845e9a61efdf811c
Vishal-Singh-Official <vishalsinghofficial.acct@gmail.com>
2025-09-18 12:10:00 +0530

    add component and make a images add

---
+++ src/component/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Hero = () => {
+  return (
+    <>
+    {/* Hero Header Starts */}
+
+    <div className="hero-section" style={{backgroundImage: "url('/images/hero-bg.jpg')"}}>
+
+    </div>
+
+     {/* Hero Header End */}
+    </>
+  )
+}
+
+export default Hero;
